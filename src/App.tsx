3781a0f00b55d03f4ea5b054501a915da1c2288f
Carolina Peneda <cdpeneda@gmail.com>
2026-03-18 08:04:27 -0300

    feat: projeto iRepair finalizado e recuperado

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,45 @@ import Header from "./components/Header"
 import NewServiceForm from "./components/NewServiceForm"
 import ServiceCard from "./components/ServiceCard"
 
+interface ServiceOrder {
+  cliente: string
+  aparelho: string
+  servico: string
+  status: string
+}
+
 function App() {
+
+  const [services, setServices] = useState<ServiceOrder[]>
+  ([{
+    cliente: "Maria Eduarda",
+    aparelho: "TV",
+    servico: "Conserto de tela",
+    status: "Aberto"
+  }])
+
+  function addService(newService: ServiceOrder) {
+    setServices([...services, newService])
+  }
+
   return (
     <>
-      <div>
+      <div className="max-w-3x1 mx-auto p-6 space-y-3 min-h-screen bg-gray-100 font-sans">
         <Header />
-        <NewServiceForm />
-        <ServiceCard />
+        <NewServiceForm addService={addService} />
+        <div className="flex gap-4 flex-wrap">
+          {services.map((service, index) => (
+            <ServiceCard
+              key={index}
+              cliente={service.cliente}
+              aparelho={service.aparelho}
+              servico={service.servico}
+              status={service.status}
+            />
+          ))}
+          </div>
       </div>
     </>
-    // <h1 className="text-3xl font-bold underline">
-    //   Hello world!
-    // </h1>
   )
 }
 
